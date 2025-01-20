e95aa7400b9485e7aee7ab8cfb3f7fff35f0753d
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-01-20 18:48:06 +0530

    UserInfo

---
+++ src/model/UserInfo.js
@@ -0,0 +1,23 @@
+class UserInfo {
+  constructor(
+    userId,
+    userName,
+    userMail,
+    userPass,
+    userAge,
+    userGender,
+    userMobile,
+    userAddress
+  ) {
+    this.userId = userId;
+    this.userName = userName;
+    this.userMail = userMail;
+    this.userPass = userPass;
+    this.userAge = userAge;
+    this.userGender = userGender;
+    this.userMobile = userMobile;
+    this.userAddress = userAddress;
+  }
+}
+
+export default UserInfo;
