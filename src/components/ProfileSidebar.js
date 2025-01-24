ac3a5023c5604dd1592304be3d1c533023015e97
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-01-24 13:36:31 +0530

    Update_Field_Set

--- src/components/ProfileSidebar.js
+++ src/components/ProfileSidebar.js
@@ -2,7 +2,16 @@ import React, { useState } from "react";
 import styles from "./ProfileSidebar.module.css";
 
 const ProfileSidebar = () => {
+  const loggedUser = JSON.parse(localStorage.getItem("logged_user"));
+
+  // Add state for each field to allow editing
   const [isEditable, setIsEditable] = useState(false);
+  const [userName, setUserName] = useState(loggedUser.userName);
+  const [userMail, setUserMail] = useState(loggedUser.userMail);
+  const [userAge, setUserAge] = useState(loggedUser.userAge);
+  const [userMobile, setUserMobile] = useState(loggedUser.userMobile);
+  const [userAddress, setUserAddress] = useState(loggedUser.userAddress);
+  const [userGender, setUserGender] = useState(loggedUser.userGender);
 
   const toggleEdit = () => {
     setIsEditable(!isEditable);
@@ -19,7 +28,8 @@ const ProfileSidebar = () => {
           <input
             type="text"
             id="name"
-            value="Burk Macklin"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
             readOnly={!isEditable}
           />
         </div>
@@ -28,20 +38,28 @@ const ProfileSidebar = () => {
           <input
             type="email"
             id="email"
-            value="abc@gmail.com"
+            value={userMail}
+            onChange={(e) => setUserMail(e.target.value)}
             readOnly={!isEditable}
           />
         </div>
         <div className={styles.formGroup}>
           <label htmlFor="age">Age</label>
-          <input type="number" id="age" value="35" readOnly={!isEditable} />
+          <input
+            type="number"
+            id="age"
+            value={userAge}
+            onChange={(e) => setUserAge(e.target.value)}
+            readOnly={!isEditable}
+          />
         </div>
         <div className={styles.formGroup}>
           <label htmlFor="mobile">Mobile</label>
           <input
             type="text"
             id="mobile"
-            value="00923469874656"
+            value={userMobile}
+            onChange={(e) => setUserMobile(e.target.value)}
             readOnly={!isEditable}
           />
         </div>
@@ -50,13 +68,22 @@ const ProfileSidebar = () => {
           <input
             type="text"
             id="address"
-            value="Street no. 4, XYZ"
+            value={userAddress}
+            onChange={(e) => setUserAddress(e.target.value)}
             readOnly={!isEditable}
           />
         </div>
         <div className={styles.formGroup}>
           <label htmlFor="gender">Gender</label>
-          <input type="text" id="gender" value="Male" readOnly={!isEditable} />
+          <select
+            id="gender"
+            value={userGender}
+            onChange={(e) => setUserGender(e.target.value)}
+            disabled={!isEditable}
+          >
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
         </div>
       </form>
       <button className={styles.editProfile} onClick={toggleEdit}>
