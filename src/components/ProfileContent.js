91e7f9f1b4e79a2c0b961a573c3115f72a9e0103
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-01-25 20:15:10 +0530

    PastAppointmentList.module.css

--- src/components/ProfileContent.js
+++ src/components/ProfileContent.js
@@ -1,10 +1,12 @@
 import FutureAppointmentList from "./FutureAppointmentList";
+import PastAppointmentList from "./PastAppointmentList";
 import styles from "./ProfileContent.module.css";
 
 const ProfileContent = () => {
   return (
     <div className={styles.profileContent}>
       <FutureAppointmentList></FutureAppointmentList>
+      <PastAppointmentList></PastAppointmentList>
     </div>
   );
 };
