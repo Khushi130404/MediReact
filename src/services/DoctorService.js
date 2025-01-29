f5301c68dcc9914446ec0f49f99edd471beaeb92
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-01-29 15:16:45 +0530

    USER_UPDATE_API

--- src/services/DoctorService.js
+++ src/services/DoctorService.js
@@ -4,6 +4,7 @@ const BASE_URL = "http://localhost:8080";
 const DOCTOR_LOGIN_API = `${BASE_URL}/doctor/login_doctor`;
 const DOCTOR_SHOW_API = `${BASE_URL}/doctor/show_doctor`;
 const DOCTOR_BY_DOCID_API = `${BASE_URL}/doctor/get_doctor`;
+const DOCTOR_UPDATE_API = `${BASE_URL}/doctor/update_doctor`;
 
 export const loginDoctor = async (mail, pass) => {
   try {
@@ -28,6 +29,17 @@ export const getDoctorById = async (docId) => {
   try {
     const doctor = await axios.post(`${DOCTOR_BY_DOCID_API}/${docId}`);
     return doctor.data;
+  } catch (error) {
+    throw error.response ? error.response.data : "Something went wrong!";
+  }
+};
+
+export const updateDoctor = async (doctorInfo) => {
+  try {
+    console.log(doctorInfo);
+    const response = await axios.post(DOCTOR_UPDATE_API, doctorInfo);
+    console.log("Hello");
+    return response.data;
   } catch (error) {
     throw error.response ? error.response.data : "Something went wrong!";
   }
