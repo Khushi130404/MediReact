695deec1acb6183ca8b097fbeee83854923331c9
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-02-11 19:39:50 +0530

    Upload Image Crash

--- src/services/DiagnosisService.js
+++ src/services/DiagnosisService.js
@@ -1,13 +1,22 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:8080";
-const DIAGNOS_ADD_API = `${BASE_URL}/doctor/addDiagnos`;
+const DIAGNOS_ADD_API = `${BASE_URL}/diagnosis/addDiagnos`;
 
-export const addDiagnosis = async (appId, image) => {
+export const addDiagnosis = async (appId, formData) => {
   try {
-    const diagnosis = await axios.post(`${DIAGNOS_ADD_API}/${appId}/${image}`);
-    return diagnosis.data;
+    console.log("In Diag Service");
+    await axios.post(`${DIAGNOS_ADD_API}/${appId}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log("Upload successful:");
   } catch (error) {
-    throw error.response ? error.response.data : "Something went wrong!";
+    console.error(
+      "Upload error:",
+      error.response?.data || "Something went wrong!"
+    );
+    throw error.response?.data || "Something went wrong!";
   }
 };
