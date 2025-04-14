a40b1f8877e377ac3d12b3e359c712e49cff9454
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-04-14 20:28:10 +0530

    Forgot Gmail CSS

---
+++ src/components/ForgotGmail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ForgotGmail = () => {
+    return (
+        <div>
+            <h1>Forgot Gmail</h1>
+            <p>Please enter your details to recover your Gmail account.</p>
+        </div>
+    );
+};
+
+export default ForgotGmail;
