66346f2cf54f6d7c9e46ac67c783a3189aeea81d
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-04-18 22:29:31 +0530

    Fetch Doc Info

--- src/components/AboutDoc.js
+++ src/components/AboutDoc.js
@@ -1,8 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./AboutDoc.module.css";
+import { findAboutInfoByDocId } from "../services/AboutService";
 
 const AboutDoc = ({ initialDoctor }) => {
   const [doctor, setDoctor] = useState(initialDoctor);
+  const [docInfo, setDocInfo] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchInfo = async () => {
+      try {
+        const info = await findAboutInfoByDocId(doctor.doctorId);
+        console.log("Fetched info:", info);
+        if (info) {
+          setDocInfo(info.about);
+        } else {
+          setDocInfo(null);
+        }
+      } catch (error) {
+        setError("Error fetching doctor information");
+        console.error("Error in try-catch block:", error);
+      }
+    };
+
+    if (doctor.doctorId) {
+      fetchInfo();
+    }
+  }, [doctor]);
 
   const getGenderImage = (gender) => {
     if (gender.toLowerCase() === "male") {
@@ -29,11 +53,18 @@ const AboutDoc = ({ initialDoctor }) => {
         </div>
         <div className={styles.info}>
           <p>
-            <strong>Specialist:</strong>
-          </p>
-          <p>
             <strong>Age:</strong> {doctor.doctorAge}
           </p>
+          {error && <p className={styles.errorMessage}>{error}</p>}
+          {docInfo ? (
+            <div>
+              <p>{docInfo}</p>
+            </div>
+          ) : docInfo === null ? (
+            <p>No additional information available for this doctor.</p>
+          ) : (
+            <p>Loading additional information...</p>
+          )}
         </div>
       </div>
     </div>
