1d5c729d1edef8f141e341ad313e5b2f630d784d
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-04-21 00:07:02 +0530

    USER_FAV_DOC_API

---
+++ src/components/UserFavDoc.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./UserFavDoc.module.css";
+
+const UserFavDoc = () => {
+  return (
+    <div>
+      <div className={styles.appointmentCard}>
+        <div className={styles.header}>
+          <strong>"Loading..."</strong>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserFavDoc;
--- src/components/UserProfileContent.js
+++ src/components/UserProfileContent.js
@@ -1,5 +1,6 @@
 import FutureAppointmentList from "./FutureAppointmentList";
 import PastAppointmentList from "./PastAppointmentList";
+import UserFavDoc from "./UserFavDoc";
 import styles from "./UserProfileContent.module.css";
 
 const UserProfileContent = () => {
@@ -7,6 +8,9 @@ const UserProfileContent = () => {
     <div className={styles.profileContent}>
       <div className={styles.profContentDiv}>
         <FutureAppointmentList></FutureAppointmentList>
+      </div>
+      <div className={styles.profContentDiv}>
+        <UserFavDoc></UserFavDoc>
       </div>
       <div className={styles.profContentDiv}>
         <PastAppointmentList></PastAppointmentList>
